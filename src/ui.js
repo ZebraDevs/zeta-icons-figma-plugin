da4d37e23add5aefd8935e6e05d7647d9203a93d
thelukewalton <lwalton@zebra.com>
2025-05-07 16:36:51 +0100

    tidy up code

--- src/ui.js
+++ src/ui.js
@@ -2,11 +2,7 @@ import "@zebra-fed/zeta-web/index.css";
 import "@lottiefiles/lottie-player";
 import * as animation from "./check.json";
 
-export const loadAnimation = () => {
-    const player = document.querySelector("lottie-player");
-    player.addEventListener("rendered", (e) => {
-        player.load(animation);
-    });
+window.loadAnimation = () => {
+    document.querySelector("lottie-player")
+        .addEventListener("rendered", e => e.target.load(animation));
 };
-
-(window).loadAnimation = loadAnimation;
